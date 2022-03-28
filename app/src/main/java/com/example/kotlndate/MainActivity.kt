eac4ce580d374c1f948e00d9de1daee8ee2be601
LeonCookie <leongdynia1105@gmail.com>
2022-03-28 15:39:57 +0200

    s

--- app/src/main/java/com/example/kotlndate/MainActivity.kt
+++ app/src/main/java/com/example/kotlndate/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlndate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
@@ -11,11 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lateinit var DayToDay: DatePicker
+        lateinit var DayToDay: EditText
         lateinit var TextToDay: TextView
+
+        lateinit var First: EditText
+        lateinit var Second: EditText
+
+
+
 
         DayToDay = findViewById(R.id.DateToDay)
         TextToDay = findViewById(R.id.textViewDay)
+
+        First = findViewById(R.id.FirstDay)
+        Second = findViewById(R.id.SecondDay)
+
+        val today = "12-121-2"// test value
+
+        //val one =
+
+
+
+        TextToDay.text= today
+
+
+
+
 
 
     }
